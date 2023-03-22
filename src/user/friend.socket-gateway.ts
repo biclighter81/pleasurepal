5e9e03b2f4573c9e5a2bc8505b53b992cee41952
biclighter81 <moritzben.becker@outlook.de>
2023-03-22 23:06:25 +0100

    online state

--- src/user/friend.socket-gateway.ts
+++ src/user/friend.socket-gateway.ts
@@ -1,3 +1,4 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import {
   ConnectedSocket,
   MessageBody,
@@ -6,6 +7,8 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { IsNull, Not, Repository } from 'typeorm';
+import { UserFriendshipRequest } from './entities/user-friendship-request.entity';
 
 @WebSocketGateway(80, {
   cors: {
@@ -20,6 +23,11 @@ export class FriendSocketGateway {
   @WebSocketServer()
   server: Server;
 
+  constructor(
+    @InjectRepository(UserFriendshipRequest)
+    private readonly userFriendshipRequestRepo: Repository<UserFriendshipRequest>,
+  ) {}
+
   @SubscribeMessage('connect')
   async handleConnection(@ConnectedSocket() client: Socket) {
     const { sub } = client.handshake.auth;
@@ -27,12 +35,54 @@ export class FriendSocketGateway {
       id: sub,
       socketId: client.id,
     });
-    client.join(sub);
+    await client.join(sub);
+    const friends = await this.getFriends(sub);
+    await this.emitOnlineFriends(friends, sub, client);
   }
 
   @SubscribeMessage('disconnect')
   async handleDisconnect(@ConnectedSocket() client: Socket) {
-    const idx = this.connectedUsers.findIndex((user) => user.socketId === client.id);
+    const idx = this.connectedUsers.findIndex(
+      (user) => user.socketId === client.id,
+    );
     this.connectedUsers.splice(idx, 1);
+  }
+
+  async emitOnlineFriends(
+    friends: UserFriendshipRequest[],
+    sub: string,
+    client: Socket,
+  ) {
+    const onlineFriends = friends.filter((f) =>
+      this.connectedUsers.find((u) => u.id === (f.to == sub ? f.from : f.to)),
+    );
+    if (onlineFriends.length) {
+      for (const friend of onlineFriends) {
+        client
+          .to(friend.to == sub ? friend.from : friend.to)
+          .emit('friend-online', {
+            uid: sub,
+          });
+      }
+      client.emit(
+        'online-friends',
+        onlineFriends.map((f) => (f.to == sub ? f.from : f.to)),
+      );
+    }
+  }
+
+  async getFriends(sub: string) {
+    const friends = await this.userFriendshipRequestRepo
+      .createQueryBuilder()
+      .where({
+        from: sub,
+        acceptedAt: Not(IsNull()),
+      })
+      .orWhere({
+        to: sub,
+        acceptedAt: Not(IsNull()),
+      })
+      .getMany();
+    return friends;
   }
 }
